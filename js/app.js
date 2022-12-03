65ed24ab7962141930579350bc8552278f2cab41
tektechnologies <info@tektechnologies.com>
2022-12-03 12:02:46 -0600

    finish html css

---
+++ js/app.js
@@ -0,0 +1,8 @@
+'use strict';
+console.log('js file connected!');
+
+let allPizzas = [];
+let pizzaForm = document.getElementById('pizzaForm');
+let pizzaList = document.getElementById('pizzaList');
+
+
